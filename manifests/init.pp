061ea9ea230ba318b86f0ec4bd24724bf6c9ff63
William Hughes <williamh@catalyst.net.nz>
2016-12-19 09:51:02 +1300

    Fix a bug where if livepatch is installed before snapd is updated,
    livepatch won't work until reinstalled

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,21 @@
 class livepatch($ensure='disabled', $token=undef) {
 
   $lp_bin = '/snap/bin/canonical-livepatch'
+
+  # If livepatch is installed before an up-to-date snapd is installed, then livepatch
+  # needs to be reinstalled before it will work
+  exec { 'remove-old-livepatch':
+    command => '/usr/bin/snap remove canonical-livepatch',
+    timeout => 0,
+    onlyif  => "/usr/bin/file ${lp_bin} && ${lp_bin} status | /bin/grep 'no such file or directory'"
+  }
+  ->
   exec { 'install-livepatch':
     command => '/usr/bin/snap install canonical-livepatch',
     timeout => 0,
     creates => $lp_bin
   }
- 
+
   if ($ensure == 'enabled') {
     if ($token == undef) {
       err('Livepatch token must be set before enabling livepatch')
